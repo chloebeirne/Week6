575f38e97dbeb9d9e930a984d4900c532a22c53b
chloe <g00399850@atu.ie>
2025-09-01 19:02:09 +0100

    Updating Product Service

--- src/main/java/ie/atu/week6/ProductService.java
+++ src/main/java/ie/atu/week6/ProductService.java
@@ -1,4 +1,32 @@
 package ie.atu.week6;
 
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+@Service
+
 public class ProductService {
+    private List<Product> myList  = new ArrayList<>();
+
+    public List<Product> addProduct(Product product){
+        myList.add(product);
+        return myList;
+
+    }
+    public List<Product> updateProduct( int id, Product product) {
+        for (Product p : myList) {
+            if (product.getId() == id) {
+                myList.remove(p);
+            }
+        }
+        myList.add(product);
+        return myList;
+    }
+    public List<Product> deleteProduct( int id, Product product) {
+        public List<Product> deleteProduct( int id) {
+            for (Product p : myList) {
+                if (product.getId() == id) {
+                    if (p.getId() == id) {
+                        myList.remove(p);
+                    }
 }
